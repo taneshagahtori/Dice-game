5a42542b0675a5f376a8d3bc0717358d5e35335c
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2023-12-05 01:21:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.random();
+randomNumber1= randomNumber1*6;
+randomNumber1= Math.floor(randomNumber1)+1;
+
+
+var randomDiceImage= "dice" + randomNumber1 + ".png";
+var randomDiceSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceSource); 
+
+var randomNumber2= Math.random();
+randomNumber2= randomNumber2*6;
+randomNumber2= Math.floor(randomNumber2)+1;
+
+var randomDiceImage2= "dice" + randomNumber2 + ".png";
+var randomDiceSource2= "images/" + randomDiceImage2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceSource2);
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML=" 🚩Player 1 wins!"
+}
+else if (randomNumber1===randomNumber2) {
+    document.querySelector("h1").innerHTML=" 🚩Draw!"
+}
+else {
+    document.querySelector("h1").innerHTML=" 🚩Player 2 wins!"
+}
